4e1fc4bfdf9fc60e032d94f3f6e6d523f1387c1f
Rodney Thompson <godneyson06@gmail.com>
2021-12-16 10:42:51 -0800

    yeet

--- index.js
+++ index.js
@@ -7,6 +7,6 @@ app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, '/public/index.html'))
 })
 
-const port = process.env.Port || 4545
+const port = process.env.PORT || 4545
 
 app.listen(port, () => console.log(`March onward to.... ${port}`))
